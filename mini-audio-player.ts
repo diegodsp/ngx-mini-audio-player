396d44b94dd17a7bebc7f3ccbe1bc9f426ab1293
Diego Pereira <diegodsp@users.noreply.github.com>
2020-10-13 11:08:39 -0300

    Create mini-audio-player.ts
    - Create the first version of component

---
+++ mini-audio-player.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'mini-audio-player',
+    template: '<i class="fa" [class.fa-download]="buffering" [class.fa-play]="!buffering && !playing" [class.fa-pause]="!buffering && playing" (click)="onClick()"></i>'
+})
+export class MiniAudioPlayerComponent implements OnInit, OnDestroy {
+    player = new Audio();
+    playing = false;
+    buffering = false;
+    @Input() url: string;
+
+    constructor() { }
+
+    /**
+     * Handle the buffering events.
+     */
+    ngOnInit(): void {
+        this.player.onloadstart = () => {
+            this.buffering = true;
+        };
+        this.player.onloadeddata = () => {
+            this.buffering = false;
+        };
+    }
+
+    /**
+     * Detect change pages or recreate component.
+     */
+    ngOnDestroy(): void {
+        this.player?.pause();
+    }
+
+    /**
+     * Handle the click to play/pause the audio.
+     */
+    onClick() {
+        if (this.playing) {
+            this.player.pause();
+        } else {
+            this.player.src = this.url;
+            this.player.load();
+            this.player.play();
+        }
+        this.playing = !this.playing;
+    }
+}
